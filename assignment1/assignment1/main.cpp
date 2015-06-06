11ada76be738c36d42b3fdb2b97438f49380de3b
Takarada issei <s782a8a8e7dkt@gmail.com>
2015-06-06 11:03:49 +0900

    使用１を実装

---
+++ assignment1/assignment1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  assignment1
+//
+//  Created by Takarada issei on 2015/06/06.
+//  Copyright (c) 2015年 Takarada issei. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    cout<<"オムライス"<<endl;
+    return 0;
+}
