d90a2d5839a913fbd9d83d603e9dd0ca3d9efbdf
Takarada issei <s782a8a8e7dkt@gmail.com>
2015-06-06 12:00:07 +0900

    使用４を実装

--- README.txt
+++ README.txt
@@ -3,4 +3,6 @@ I use C++ today.
 実行手順
 1.コマンドライン上でcd [filename]でpush
 2.もう一度cd [filename]でpush
-3.c++ main.cppと入力するとプログラムが開始され、'オムライス'と表示されます。
+3.c++ main.cppと入力するとプログラムが開始され、recipe-data.txtに’オムライス'と書き込まれます。
+4. cat recipe-data.txtで記述内容が確認できます。
+5.
--- assignment1/assignment1/main.cpp
+++ assignment1/assignment1/main.cpp
@@ -10,19 +10,31 @@
 #include <fstream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    
-    fstream data_file;
+void insertWords(ofstream &recipe_file){
+    recipe_file.open("/Users/issei/issei-sezemi-2015-readable-code/recipe-data.txt");
+    recipe_file<<"オムライス"<<endl;
+    recipe_file<<"親子丼"<<endl;
+    recipe_file<<"杏仁豆腐"<<endl;
+}
+
+void initFile(fstream &data_file){
     data_file.open("/Users/issei/issei-sezemi-2015-readable-code/recipe-data.txt");
     
     if (!data_file) {
         cout<<"ファイルをオープンできません"<<endl;
         exit(0);
     }
+
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    fstream data_file;
+    initFile(data_file);
     
     ofstream recipe_file;
-    recipe_file.open("/Users/issei/issei-sezemi-2015-readable-code/recipe-data.txt");
-    recipe_file<<"オムライス"<<endl;
+    insertWords(recipe_file);
     
     recipe_file.close();
     data_file.close();
