8610618810074b517d06ca7f946cdaccde7f0830
Narishi Chida <nariyoshichidamm@gmail.com>
2015-06-06 14:31:01 +0900

    added .gitignore

--- assignment1/assignment1/main.cpp
+++ assignment1/assignment1/main.cpp
@@ -6,25 +6,42 @@
 //  Copyright (c) 2015年 Takarada issei. All rights reserved.
 //
 
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 void insertWords(ofstream &recipe_file){
-    recipe_file.open("/Users/issei/issei-sezemi-2015-readable-code/recipe-data.txt");
+    recipe_file.open("recipe-data.txt");
     recipe_file<<"オムライス"<<endl;
     recipe_file<<"親子丼"<<endl;
     recipe_file<<"杏仁豆腐"<<endl;
 }
 
 void initFile(fstream &data_file){
-    data_file.open("/Users/issei/issei-sezemi-2015-readable-code/recipe-data.txt");
+    data_file.open("recipe-data.txt");
     
     if (!data_file) {
         cout<<"ファイルをオープンできません"<<endl;
         exit(0);
     }
+}
 
+void addIdentifierToWords(ifstream &input_file,string &filename){
+
+  input_file.open(filename.c_str());
+
+  if( !input_file ) {
+    cout<<"ファイルをオープンできません"<<endl;
+    exit(0);
+  }
+
+  int identifier = 1;
+  string words;
+  while( !( input_file >> words ).fail() ){
+    cout << identifier << ": " << words << endl;
+    ++identifier;
+  }
+  
 }
 
 
@@ -35,7 +52,14 @@ int main(int argc, const char * argv[]) {
     
     ofstream recipe_file;
     insertWords(recipe_file);
+
+    assert( argc >= 2 ); 
+    string filename = string(argv[1]);
+    ifstream input_file;
+    addIdentifierToWords(input_file,filename);
     
     recipe_file.close();
     data_file.close();
+    input_file.close();
+
 }
